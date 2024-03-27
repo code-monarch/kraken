991ba51c99264fdc8dd0b100c7b70bb0ea8949cc
theblessingokonkwo <theblessingokonkwo@gmail.com>
2024-03-27 14:54:25 +0100

    api services endpoints

---
+++ src/redux/services/activity-logs/activities.api-slice.ts
@@ -0,0 +1,107 @@
+import { baseApiSlice } from "@/redux/api/base-api";
+
+export interface IActivities {
+  error: boolean;
+  responseCode: string;
+  data: {
+    _id: string;
+    user: string;
+    title: string;
+    activity: string;
+    createdAt: string;
+    __v: number;
+  }[];
+}
+
+export interface ISingleActivity {
+  error: boolean;
+  responseCode: string;
+  data: {
+    _id: string;
+    user: string;
+    title: string;
+    activity: string;
+    createdAt: string;
+    __v: number;
+  };
+}
+
+export interface IDeleteActivitiesRes {
+  error: boolean;
+  responseCode: string;
+  data: {
+    acknowledged: boolean;
+    deletedCount: number;
+  };
+}
+
+export interface IDeleteSingleActivityRes {
+  error: boolean;
+  responseCode: string;
+  data: {
+    _id: string;
+    user: string;
+    title: string;
+    activity: string;
+    createdAt: string;
+    __v: number;
+  };
+}
+
+interface IActivityPayload {
+  id: string;
+}
+
+export const activitiesApiSlice = baseApiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getActivities: builder.query<IActivities, IActivityPayload>({
+      query: ({ id }) => ({
+        url: `settings/admin/activities/${id}`,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+    }),
+
+    deleteActivities: builder.mutation<IDeleteActivitiesRes, IActivityPayload>({
+      query: ({ id }) => ({
+        url: `settings/admin/activities/${id}`,
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+    }),
+
+    getSingleActivity: builder.query<ISingleActivity, IActivityPayload>({
+      query: ({ id }) => ({
+        url: `settings/admin/activity/${id}`,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+    }),
+
+    deleteSingleActivity: builder.mutation<
+      IDeleteSingleActivityRes,
+      IActivityPayload
+    >({
+      query: ({ id }) => ({
+        url: `settings/admin/activity/${id}`,
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }),
+    }),
+
+  }),
+});
+
+export const {
+  useGetActivitiesQuery,
+  useDeleteActivitiesMutation,
+  useDeleteSingleActivityMutation,
+} = activitiesApiSlice;
