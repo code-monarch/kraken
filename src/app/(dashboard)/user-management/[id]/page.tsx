bb1ce9639ec81d40594b0051a75e26bc5a9fa8d5
Joel Adie <adiejoel35@gmail.com>
2024-05-25 18:14:08 +0100

    fix: Remove getStatic props

--- src/app/(dashboard)/user-management/[id]/page.tsx
+++ src/app/(dashboard)/user-management/[id]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import React from "react";
+import { useParams } from 'next/navigation'
 import GoBackWidget from "@/pattern/common/molecules/data-display/go-back-widget";
 import PilgrimDetailsTemplate from "@/pattern/user-management/templates/pilgrim-details-template";
 
-const UserDetailsPage = ({ params }: { params: { id: string } }) => {
-  
+const UserDetailsPage = () => {
+  const params = useParams<{ id: string }>()
+
   return (
     <>
       <GoBackWidget page="User details" />
@@ -12,7 +14,5 @@ const UserDetailsPage = ({ params }: { params: { id: string } }) => {
     </>
   );
 };
-
-export const generateStaticParams = () => { id: 2 };
 
 export default UserDetailsPage;
