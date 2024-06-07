e5b4dac42ad82d1f252a80dcb92a88a1eda669d8
Joel Adie <adiejoel35@gmail.com>
2024-06-07 02:57:41 +0100

    fix: Resolve gitlab build error

--- src/lib/helper/logOut.ts
+++ src/lib/helper/logOut.ts
@@ -2,19 +2,12 @@ import LocalStore from './storage-manager'
 import { AUTH_PATHS } from '../routes'
 import { toast } from 'sonner'
 import { baseApiSlice } from '@/redux/api/base-api'
-import { ILogoutResponse } from '@/redux/types'
-import { Action, EnhancedStore, StoreEnhancer, ThunkAction, ThunkDispatch, Tuple, UnknownAction } from '@reduxjs/toolkit'
-import { MutationActionCreatorResult, MutationDefinition, BaseQueryFn, FetchArgs, FetchBaseQueryError, CombinedState } from '@reduxjs/toolkit/query'
-import { IAuthState } from '@/redux/slices/auth'
-import { IGlobalState } from '@/redux/slices/global'
-import { IUserSlice } from '@/redux/slices/user-slice'
-import { PersistPartial } from 'redux-persist/es/persistReducer'
 
-// let store: { dispatch: (arg0: ThunkAction<MutationActionCreatorResult<MutationDefinition<void, BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError>, "getUser" | "getAdmin" | "getProfile", ILogoutResponse, "baseApi">>, any, any, UnknownAction>) => Promise<any> }
+let store: any
 
-// export const injectStore = (_store: EnhancedStore<{ baseApi: CombinedState<{ logout: MutationDefinition<void, BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError>, "getUser" | "getAdmin" | "getProfile", ILogoutResponse, "baseApi"> }, "getUser" | "getAdmin" | "getProfile", "baseApi">; globalState: IGlobalState & PersistPartial; authState: IAuthState & PersistPartial; userDetails: IUserSlice & PersistPartial } & PersistPartial, Action, Tuple<[StoreEnhancer<{ dispatch: ThunkDispatch<{ baseApi: CombinedState<{ logout: MutationDefinition<void, BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError>, "getUser" | "getAdmin" | "getProfile", ILogoutResponse, "baseApi"> }, "getUser" | "getAdmin" | "getProfile", "baseApi">; globalState: IGlobalState & PersistPartial; authState: IAuthState & PersistPartial; userDetails: IUserSlice & PersistPartial } & PersistPartial, undefined, UnknownAction> }>, StoreEnhancer]>>) => {
-//   store = _store
-// }
+export const injectStore = (_store: unknown) => {
+  store = _store
+}
 
 export const clearLocalStorage = () => {
   LocalStore.clearStore()
@@ -24,12 +17,12 @@ export const clearLocalStorage = () => {
 }
 
 export const logoutAndClearStorage = async () => {
-  // await store
-  //   .dispatch(baseApiSlice.endpoints.logout.initiate())
-    .then((res) => {
+  await store
+    .dispatch(baseApiSlice.endpoints.logout?.initiate())
+    .then((res: any) => {
       console.log("LOG OUT RESPONSE: ", res)
       clearLocalStorage()
-    }).catch((err) => {
+    }).catch((err: { data: { responseMessage: any } }) => {
       toast.error('Unexpected error', {
         description: `${err?.data?.responseMessage ??
           'We encountered an error while logging you out, Please try again'
