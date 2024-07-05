130d723bb9a938a7ca993e6d1158856a39cb5502
theblessingokonkwo <theblessingokonkwo@gmail.com>
2024-07-05 10:55:08 +0100

    integrate cashout

--- src/redux/services/transactions/get-cashout-requests.api-slice.ts
+++ src/redux/services/transactions/get-cashout-requests.api-slice.ts
@@ -1,56 +1,95 @@
 import { baseApiSlice } from '@/redux/api/base-api'
 
 interface ICashoutRequest {
-    id: string,
-    userid: string,
-    description: string,
-    type: string,
-    reference: string,
-    sessionID: string,
-    order_amount: number,
-    fee: number,
-    currency: "NGN",
-    status: "PENDING" | "APPROVED" | "DECLINED",
-    metadata: string,
-    createdAt: string,
-    updatedAt: string,
-    history: {
-        id: string,
-        walletid: string,
-        transactionid: string,
-        amount: string,
-        initial_balance: number,
-        balance_after: number,
-        type: "debit",
-        is_reversed: null,
-        createdAt: string,
-        updatedAt: string
-    }
+  id: string
+  userid: string
+  walletid: string
+  transactionid: string
+  approved_by: null
+  status: string
+  amount: number
+  currency: string
+  matadata: null
+  createdAt: string
+  updatedAt: string
+  transaction: {
+    id: string
+    userid: string
+    description: string
+    type: string
+    reference: string
+    sessionID: string
+    order_amount: number
+    fee: number
+    currency: string
+    status: string
+    metadata: null
+    createdAt: string
+    updatedAt: string
+  }
+  wallet: {
+    id: string
+    userid: string
+    currency: string
+    type: string
+    available_balance: number
+    ledger_balance: number
+    is_frozen: boolean
+    createdAt: string
+    updatedAt: string
+  }
+}
+
+interface ISingleCashoutRequestResponse {
+  error: boolean
+  responseCode: string
+  responseMessage: string
+  data: ICashoutRequest
 }
 
 interface IGetCashoutRequestsResponse {
-    error: boolean,
-    responseCode: string,
-    responseMessage: string,
-    data: ICashoutRequest[]
+  error: boolean
+  responseCode: string
+  responseMessage: string
+  data: ICashoutRequest[]
+}
+
+interface ICashoutRequestPayload {
+  id: string
 }
 
 export const getCashoutRequestsApiSlice = baseApiSlice.injectEndpoints({
-    endpoints: builder => ({
-        getCashoutRequests: builder.query<IGetCashoutRequestsResponse, void>(
-            {
-                query: () => ({
-                    url: `transactions/admin/cashout`,
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    keepUnusedDataFor: 5,
-                }),
-                providesTags: ['getCashoutRequests'],
-            },
-        ),
+  endpoints: builder => ({
+    getCashoutRequests: builder.query<IGetCashoutRequestsResponse, void>({
+      query: () => ({
+        url: `transactions/admin/cashout`,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        keepUnusedDataFor: 5,
+        mode: 'no-cors',
+      }),
+      providesTags: ['getCashoutRequests'],
     }),
+
+    getSingleCashoutRequest: builder.query<
+      ISingleCashoutRequestResponse,
+      ICashoutRequestPayload
+    >({
+      query: ({ id }) => ({
+        url: `transactions/admin/cashout/${id}`,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        keepUnusedDataFor: 5,
+        mode: 'no-cors',
+      }),
+      providesTags: ['getCashoutRequests'],
+    }),
+  }),
 })
 
-export const { useGetCashoutRequestsQuery } = getCashoutRequestsApiSlice
+export const { useGetCashoutRequestsQuery, useGetSingleCashoutRequestQuery } =
+  getCashoutRequestsApiSlice
